a9b9c2d13f0bd0d6061613d5fbaba76f04c94365
ShunDeveloper <s.tsuchiya.business@gmail.com>
2022-10-12 19:46:35 +0900

    計算テストでnanを出力しないようにしました

--- pages/2_🀇_点数計算テスト(Easy).py
+++ pages/2_🀇_点数計算テスト(Easy).py
@@ -9,9 +9,28 @@ st.set_page_config(
     page_icon="🀐",
 )
 
+# textがnaならTrueを返す
+def isNA(text):
+    flag = False
+    if text=='<NA>':
+        #print('NA founded')
+        flag=True
+    if type(text)==float:
+        flag=True
+        #print('NA founded')
+    return flag
+
 ### session
 if 'answer_easy' not in st.session_state:
-    st.session_state['question_easy'], st.session_state['answer_easy'] = initialize(mode='easy')
+    tmp_question, tmp_answer= initialize(mode='easy')
+    #print(tmp_question,tmp_answer,type(tmp_answer))
+    if isNA(tmp_answer):
+        while isNA(tmp_answer):
+            #print('looping')
+            tmp_question, tmp_answer= initialize(mode='easy')
+            #print(tmp_question,tmp_answer,type(tmp_answer),'looping')
+        st.session_state['question_easy'], st.session_state['answer_easy'] = tmp_question, tmp_answer
+    st.session_state['question_easy'], st.session_state['answer_easy'] = tmp_question, tmp_answer
 
 
 ### UI
@@ -23,4 +42,12 @@ agree = st.checkbox('答えを確認する',key='easy')
 if agree:
     st.write('チェックボックスを外すと次の問題へ行きます')
     st.write('答え:',st.session_state['answer_easy'])
-    st.session_state['question_easy'], st.session_state['answer_easy'] = initialize(mode='easy')
+    tmp_question, tmp_answer= initialize(mode='easy')
+    #print(tmp_question,tmp_answer,type(tmp_answer))
+    if isNA(tmp_answer):
+        while isNA(tmp_answer):
+            #print('looping')
+            tmp_question, tmp_answer= initialize(mode='easy')
+            #print(tmp_question,tmp_answer,type(tmp_answer),'looping')
+        st.session_state['question_easy'], st.session_state['answer_easy'] = tmp_question, tmp_answer
+    st.session_state['question_easy'], st.session_state['answer_easy'] = tmp_question, tmp_answer
