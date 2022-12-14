244353ccffe394426bbb3b0391f985673fb1d11f
ShunDeveloper <s.tsuchiya.business@gmail.com>
2022-12-14 19:29:48 +0900

    initialize.pyのrefactoring

--- question/initialize.py
+++ question/initialize.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import random
 
-data_dir = 'data/'
-ko_ron_df = pd.read_csv(data_dir+'ko_ron.csv', index_col=0, dtype='str')
-ko_tumo_df = pd.read_csv(data_dir+'ko_tumo.csv', index_col=0, dtype='str')
-oya_ron_df = pd.read_csv(data_dir+'oya_ron.csv', index_col=0, dtype='str')
-oya_tumo_df = pd.read_csv(data_dir+'oya_tumo.csv', index_col=0, dtype='str')
-oya_tumo_df = oya_tumo_df.fillna('0').applymap(lambda x: '<NA>' if x == '0' else str(x)+' all')
+DATA_DIR = 'data/'
+KO_RON_DF = pd.read_csv(DATA_DIR+'ko_ron.csv', index_col=0, dtype='str')
+KO_TUMO_DF = pd.read_csv(DATA_DIR+'ko_tumo.csv', index_col=0, dtype='str')
+OYA_RON_DF = pd.read_csv(DATA_DIR+'oya_ron.csv', index_col=0, dtype='str')
+OYA_TUMO_DF = pd.read_csv(DATA_DIR+'oya_tumo.csv', index_col=0, dtype='str')
+OYA_TUMO_DF = OYA_TUMO_DF.fillna('0').applymap(lambda x: '<NA>' if x == '0' else str(x)+' all')
 
 def initialize(mode):
     oya_or_ko = random.choice(['親','子'])
@@ -24,11 +24,11 @@ def initialize(mode):
         fu = random.choice(fu_list)
     question = str(han) + '翻' + str(fu) + '符, ' + oya_or_ko + 'の' + tumo_or_ron + '和了'
     if oya_or_ko == '親' and tumo_or_ron == 'ロン':
-        answer = oya_ron_df.iloc[han-1,:][str(fu)]
+        answer = OYA_RON_DF.iloc[han-1,:][str(fu)]
     if oya_or_ko == '親' and tumo_or_ron == 'ツモ':
-        answer = oya_tumo_df.iloc[han-1,:][str(fu)]
+        answer = OYA_TUMO_DF.iloc[han-1,:][str(fu)]
     if oya_or_ko == '子' and tumo_or_ron == 'ロン':
-        answer = ko_ron_df.iloc[han-1,:][str(fu)]
+        answer = KO_RON_DF.iloc[han-1,:][str(fu)]
     if oya_or_ko == '子' and tumo_or_ron == 'ツモ':
-        answer = ko_tumo_df.iloc[han-1,:][str(fu)]
+        answer = KO_TUMO_DF.iloc[han-1,:][str(fu)]
     return question, answer
