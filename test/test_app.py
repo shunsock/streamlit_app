3117fe0531f83eccdbd631a854a0e6b9d212928a
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2023-01-29 17:03:00 +0900

    added linter

--- test/test_app.py
+++ test/test_app.py
@@ -1,44 +1,47 @@
 import os
+import re
+import time
 
 import requests
-import time
-import re
 
-ROOT_URL = 'http://localhost:8501'
+ROOT_URL = "http://localhost:8501"
 
 PAGE_PATH = "pages/"
 PAGE_NAMES = []
-tmp = os.listdir(PAGE_PATH) # get file name
-assert 0 < len(tmp) # confirm
+tmp = os.listdir(PAGE_PATH)  # get file name
+assert 0 < len(tmp)  # confirm
 for i in range(len(tmp)):
     file_name = tmp[i]
-    file_name = re.sub(r'[0-9]+_', '', file_name)
-    file_name = re.sub(r'.py', '', file_name)
+    file_name = re.sub(r"[0-9]+_", "", file_name)
+    file_name = re.sub(r".py", "", file_name)
     PAGE_NAMES.append(file_name)
+
 
 def test_app_root():
     print()
-    print('WORKING TEST: root page')
+    print("WORKING TEST: root page")
     time.sleep(2)
     res = requests.get(ROOT_URL)
     res = str(res)
-    assert '<Response [200]>' == res
-    print('='*30)
+    assert "<Response [200]>" == res
+    print("=" * 30)
 
-def test_app_page():
-    print('WORKING TEST: page under pages dir')
+
+def test_app_page_200():
+    print("WORKING TEST: page under pages dir")
     for page_name in PAGE_NAMES:
-        print('page name:', page_name)
+        print("page name:", page_name)
         time.sleep(2)
-        res = requests.get(ROOT_URL+'/'+page_name)
+        res = requests.get(ROOT_URL + "/" + page_name)
         res = str(res)
-        assert '<Response [200]>' == res
-    print('='*30)
+        assert "<Response [200]>" == res
+    print("=" * 30)
 
-def test_app_page():
-    print('WORKING TEST: Not Found Request')
+
+def test_app_page_404():
+    print("WORKING TEST: Not Found Request")
     time.sleep(2)
-    res = requests.get(ROOT_URL+'/hoge')
+    res = requests.get(ROOT_URL + "/hoge")
     res = str(res)
-    assert '<Response [404]>' == res
-    print('='*30)
+    assert "<Response [404]>" == res
+    print("=" * 30)
--- utils/is_na.py
+++ utils/is_na.py
@@ -1,8 +1,8 @@
 # textがnaならTrueを返す
 def is_na(text: str) -> bool:
     flag = False
-    if text=='<NA>':
-        flag=True
-    if type(text)==float:
-        flag=True
+    if text == "<NA>":
+        flag = True
+    if type(text) == float:
+        flag = True
     return flag
